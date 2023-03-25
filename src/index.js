8dacc47f6d974d3a4d22af79ba8b48f5d312dd0d
tTanya98 <chichkinat98@gmail.com>
2023-03-25 20:55:29 +0200

    Update tsk

--- src/index.js
+++ src/index.js
@@ -4,3 +4,77 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchField = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onSearchCountry(e) {
+    e.preventDefault();
+    let inputCountry = e.target.value.trim();
+    if(inputCountry) {
+        return fetchCountries(inputCountry)
+        .then(data => {
+            chooseMarkup(data);
+        })
+        .catch(error => {
+            Notify.failure('Oops, there is no country with that name');
+        });
+    }
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+}
+
+function chooseMarkup(countryArray) {
+    if (countryArray.length === 1) {
+        countryList.innerHTML = '';
+        return markupCountry(countryArray);
+    }
+    if (countryArray.length >= 2 && countryArray.length <= 10) {
+        countryInfo.innerHTML = '';
+        return markupCountryItm(countryArray);
+    }
+    return Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+function markupCountryItm(data) {
+    const markup = data
+    .map(el => {
+        return `<li class="country-list_item">
+                <img class="country-list_img" src="${el.flags.svg}" alt="${el.name.official}" />
+                <p>${el.name.official}</p>
+                </li>`;
+    })
+    .join('');
+    countryList.innerHTML = markup;
+}
+
+function markupCountry(data) {
+    const markup = data
+    .map(el => {
+        return `<h1>
+        <img class="country-list_img" 
+        src="${el.flags.svg}" 
+        alt="${el.name.official}" />
+        ${el.name.official}
+        </h1>
+        <ul class="country-info_list">
+            <li class="country-info_item">
+            <h2>Capital:</h2>
+            <p>${el.capital}</p>
+            </li>
+            <li class="country-info_item">
+            <h2>Population:</h2>
+            <p>${el.population}</p>
+            </li>
+            <li class="country-info_item">
+            <h2>Languages:</h2>
+            <p>${Object.values(el.languages).join(', ')}</p>
+            </li>
+        </ul>`;
+    })
+    .join('');
+    countryInfo.innerHTML = markup;
+}
